453bc73565a21089f50d13db4eee4abf0ce1bcb1
twozeronine <amdusias209@gmail.com>
2021-02-26 16:54:49 +0900

    배포 수정

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const api = require("./routes/index");
-const cors = require("cors");
+// const cors = require("cors");
 const bodyParser = require("body-parser");
 const schema = require("./Schema/index");
 const { graphqlHTTP } = require("express-graphql");
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", api);
 
